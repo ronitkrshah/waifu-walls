a00e439587c02214630352b2390a17262571491a
RKS <ronitkrshah@tuta.io>
2024-07-04 22:57:04 +0530

    Add Settings Button

--- src/components/shared/AppHeader/ActionButtons/AccountInfo/AccountInfoDialog/DialogUserActions/index.tsx
+++ src/components/shared/AppHeader/ActionButtons/AccountInfo/AccountInfoDialog/DialogUserActions/index.tsx
@@ -1,5 +1,7 @@
+import {StyleSheet, View} from 'react-native';
 import {TAccountInfoObject} from '..';
 import LoginLogoutBtn from './LoginLogoutBtn';
+import SettingsBtn from './SettingsBtn';
 
 type Props = {
   getObject: () => TAccountInfoObject;
@@ -9,18 +11,18 @@ function DialogUserActions({getObject}: Props) {
   return (
     <>
       <LoginLogoutBtn getObject={getObject} />
-      {/* <View style={styles.container}> */}
-      {/*   <SettingsBtn getObject={getObject} /> */}
-      {/* </View> */}
+      <View style={styles.container}>
+        <SettingsBtn getObject={getObject} />
+      </View>
     </>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     marginVertical: 10,
-//     gap: 10,
-//   },
-// });
-//
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 10,
+    gap: 10,
+  },
+});
+
 export default DialogUserActions;
