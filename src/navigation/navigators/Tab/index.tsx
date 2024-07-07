4b1fa191e59c47d0b2ceb652657e3f7869036a9e
RKS <ronitkrshah@tuta.io>
2024-07-07 11:39:43 +0530

    Use outline icons for tab navigation if current tab isn't focused

--- src/navigation/navigators/Tab/index.tsx
+++ src/navigation/navigators/Tab/index.tsx
@@ -4,6 +4,7 @@ import {MD3Theme, withTheme} from 'react-native-paper';
 import HomeTab from '@app/screens/Tab/HomeTab';
 import SearchTab from '@app/screens/Tab/SearchTab';
 import AccountTab from '@app/screens/Tab/AccountTab';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 type Props = {
   theme: MD3Theme;
@@ -17,12 +18,33 @@ function TabNavigation({theme}: Props) {
       theme={theme}
       backBehavior="none"
       shifting
-      sceneAnimationType="shifting">
+      sceneAnimationType="shifting"
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color}) => {
+          let currentRoute = route.name;
+          let iconName = '';
+
+          switch (currentRoute) {
+            case 'HomeTab':
+              iconName = focused ? 'home' : 'home-outline';
+              break;
+            case 'SearchTab':
+              iconName = 'magnify';
+              break;
+            case 'AccountTab':
+              iconName = focused ? 'account' : 'account-outline';
+              break;
+          }
+
+          return (
+            <MaterialCommunityIcons name={iconName} color={color} size={24} />
+          );
+        },
+      })}>
       <Tab.Screen
         name="HomeTab"
         component={HomeTab}
         options={{
-          tabBarIcon: 'home',
           tabBarLabel: 'Home',
         }}
       />
@@ -30,7 +52,6 @@ function TabNavigation({theme}: Props) {
         name="SearchTab"
         component={SearchTab}
         options={{
-          tabBarIcon: 'magnify',
           tabBarLabel: 'Search',
         }}
       />
@@ -38,7 +59,6 @@ function TabNavigation({theme}: Props) {
         name="AccountTab"
         component={AccountTab}
         options={{
-          tabBarIcon: 'account',
           tabBarLabel: 'Account',
         }}
       />
