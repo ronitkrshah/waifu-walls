de9e4e46deb85050ff35ea8d545a46dd56f4929c
RKS <ronitkrshah@tuta.io>
2024-07-10 23:43:23 +0530

    Fix type in user home screen

--- src/screens/Tab/HomeTab/index.tsx
+++ src/screens/Tab/HomeTab/index.tsx
@@ -3,6 +3,7 @@ import UploadImageFAB from '@app/components/common/UploadImageFAB';
 import ShowWallpapers from '@app/components/screens/Tab/HomeTab/ShowWallpapers';
 import AppHeader from '@app/components/shared/AppHeader';
 import {setUserGlobalStore} from '@app/store/reducers/userReducer';
+import {IDatabaseUser} from '@app/types/wallpaper';
 import {useEffect} from 'react';
 import {Query} from 'react-native-appwrite';
 import {useDispatch} from 'react-redux';
@@ -12,16 +13,18 @@ function HomeTab() {
   useEffect(() => {
     authService
       .getCurrentUser([Query.select(['userId', 'role', 'name', 'email'])])
-      .then((user) => {
-        dispatch(
-          setUserGlobalStore({
-            userId: user.userId,
-            role: user.role,
-            name: user.name,
-            email: user.email,
-          }),
-        );
-      })
+      .then(
+        (user: Pick<IDatabaseUser, 'name' | 'role' | 'email' | 'userId'>) => {
+          dispatch(
+            setUserGlobalStore({
+              userId: user.userId,
+              role: user.role,
+              name: user.name,
+              email: user.email,
+            }),
+          );
+        },
+      )
       .catch(() => {});
   }, [dispatch]);
   return (
