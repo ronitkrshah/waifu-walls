ec71112aabcbbc25765165d8063ce773bc59d4a8
RKS <ronitkrshah@tuta.io>
2024-08-01 18:13:37 +0530

    Initial Project

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  extends: '@react-native',
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) Ronit Kr Shah
+ *
+ * The source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+function App() {
+  return null;
+}
+
+export default App;
