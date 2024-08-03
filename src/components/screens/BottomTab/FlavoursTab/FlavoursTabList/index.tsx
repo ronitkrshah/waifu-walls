d640d6d91af4edbb6cf8022623346824a65d8b70
RKS <ronitkrshah@tuta.io>
2024-08-03 12:08:28 +0530

    refactor: Use FlatList to render Flavours list in FlavoursTab

--- src/components/screens/BottomTab/FlavoursTab/FlavoursTabList/index.tsx
+++ src/components/screens/BottomTab/FlavoursTab/FlavoursTabList/index.tsx
@@ -6,39 +6,41 @@
  */
 
 import {DefaultStyles} from '@app/utils/constants/style';
-import {Dimensions, StyleSheet, View} from 'react-native';
+import {Dimensions, FlatList, StyleSheet} from 'react-native';
 import {Button} from 'react-native-paper';
 
 type Props = {
   list: string[];
 };
 
-const {width: SCREEN_WIDTH, height: SCREN_HEIGHT} = Dimensions.get('screen');
+const {width: SCREEN_WIDTH} = Dimensions.get('screen');
 
 function FlavoursTabList({list}: Props) {
   return (
-    <View style={styles.rootContainer}>
-      {list.map((item, index) => (
-        <Button
-          key={`${index}-${item}`}
-          mode={index % 2 === 0 ? 'contained-tonal' : 'text'}>
+    <FlatList
+      data={list}
+      numColumns={SCREEN_WIDTH > 700 ? 5 : 3}
+      columnWrapperStyle={styles.columnWrapper}
+      contentContainerStyle={styles.rootContainer}
+      renderItem={({item, index}) => (
+        <Button mode={index % 2 === 0 ? 'contained-tonal' : 'text'}>
           {item}
         </Button>
-      ))}
-    </View>
+      )}
+      keyExtractor={item => item}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   rootContainer: {
     width: SCREEN_WIDTH,
-    minHeight: SCREN_HEIGHT,
     paddingHorizontal: DefaultStyles.SPACING * 2,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
     gap: DefaultStyles.SPACING,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  columnWrapper: {
+    gap: DefaultStyles.SPACING,
   },
 });
 
