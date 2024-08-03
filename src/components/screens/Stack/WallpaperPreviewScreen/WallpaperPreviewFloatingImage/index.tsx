b8f47b26f36f7057a5473d6deaf1bf77c50d5c3e
RKS <ronitkrshah@tuta.io>
2024-08-04 04:00:18 +0530

    wip: Floating Image based on accelerometer

--- src/components/screens/Stack/WallpaperPreviewScreen/WallpaperPreviewFloatingImage/index.tsx
+++ src/components/screens/Stack/WallpaperPreviewScreen/WallpaperPreviewFloatingImage/index.tsx
@@ -7,9 +7,20 @@
 
 import {LatestWallpaperDTO} from '@app/features/latestWallpaperList/dto';
 import {DefaultStyles} from '@app/utils/constants/style';
-import {Dimensions, StyleSheet, View} from 'react-native';
+import {useEffect} from 'react';
+import {Dimensions, StyleSheet} from 'react-native';
 import FastImage from 'react-native-fast-image';
-import Animated, {FadeIn} from 'react-native-reanimated';
+import Animated, {
+  FadeIn,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
+import {
+  SensorTypes,
+  accelerometer,
+  setUpdateIntervalForType,
+} from 'react-native-sensors';
 
 type Props = {
   wallpaper: LatestWallpaperDTO;
@@ -17,17 +28,39 @@ type Props = {
 
 const {width: SCREEN_WIDTH} = Dimensions.get('screen');
 
+setUpdateIntervalForType(SensorTypes.accelerometer, 5);
+
 const AImage = Animated.createAnimatedComponent(FastImage);
 
 function WallpaperPreviewFloatingImage({wallpaper}: Props) {
+  const accXValue = useSharedValue(0);
+  const accYValue = useSharedValue(0);
+
+  useEffect(() => {
+    const subscribe = accelerometer.subscribe(({x, y}) => {
+      accXValue.value = Number(x.toFixed(2));
+      accYValue.value = Number(y.toFixed(2));
+    });
+    return () => subscribe.unsubscribe();
+  }, [accXValue, accYValue]);
+
+  /** Animated Style Based On Accelerometer */
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      {perspective: SCREEN_WIDTH},
+      {rotateY: `${interpolate(accXValue.value, [-8, 8], [15, -15])}deg`},
+      {rotateX: `${interpolate(accYValue.value, [-8, 8], [15, -5])}deg`},
+    ],
+  }));
+
   return (
-    <View style={styles.container}>
+    <Animated.View style={[styles.container, animatedStyle]}>
       <AImage
         entering={FadeIn.duration(800)}
         source={{uri: wallpaper.preview_url}}
-        style={[StyleSheet.absoluteFillObject, styles.image]}
+        style={[StyleSheet.absoluteFillObject]}
       />
-    </View>
+    </Animated.View>
   );
 }
 
@@ -35,9 +68,8 @@ const styles = StyleSheet.create({
   container: {
     width: SCREEN_WIDTH * 0.7,
     height: SCREEN_WIDTH * 1.2,
-  },
-  image: {
     borderRadius: DefaultStyles.SPACING * 2,
+    overflow: 'hidden',
   },
 });
 
