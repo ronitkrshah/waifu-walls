384f8b0adec38c9e157fd5463c1aff501e0841c5
RKS <ronitkrshah@tuta.io>
2024-08-10 02:45:04 +0530

    drop: Disable Admin features for now

--- src/components/screens/BottomTab/AccountTab/AccountTabUserDashboard/index.tsx
+++ src/components/screens/BottomTab/AccountTab/AccountTabUserDashboard/index.tsx
@@ -29,7 +29,7 @@ function AccountTabUserDashboard() {
           variant="headlineMedium">
           {user.name}
         </Text>
-        <AdminsOnlyFeatures />
+        {/** <AdminsOnlyFeatures /> */}
         <LogoutHandler />
       </Surface>
     </Fragment>
