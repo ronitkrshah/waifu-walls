09ad7932ee3c4f1f3ea4eb2346aa889609af63d0
RKS <ronitkrshah@tuta.io>
2024-08-10 12:42:38 +0530

    feat: Set minimum size require for compression is 200KB and maximum image upload limit is 1MB

--- src/features/uploadWallpaper/services/UploadWallpaperService.ts
+++ src/features/uploadWallpaper/services/UploadWallpaperService.ts
@@ -9,6 +9,7 @@ import {Image as Compressor} from 'react-native-compressor';
 import UploadWallpaperRepository from '../domain/repositories/UploadWallpaperRepository';
 import {UploadWallpaperProps} from '../types';
 import * as FileSystem from '@dr.pogodin/react-native-fs';
+import {WallpaperLimits} from '@app/utils/constants/limits';
 
 class UploadWallpaperService {
   private _repo: UploadWallpaperRepository;
@@ -21,12 +22,34 @@ class UploadWallpaperService {
    * Upload Image
    */
   async uploadWallpaper(props: UploadWallpaperProps) {
-    const compressedImage = await Compressor.compress(props.imagePath);
-    const stats = await FileSystem.stat(compressedImage);
+    const originalFileStats = await FileSystem.stat(props.imagePath);
+    let imageSize = originalFileStats.size;
+    let localImagePath = props.imagePath;
+
+    /**
+     * If Image Size is above 200KB then compress that image
+     */
+    if (
+      imageSize >= WallpaperLimits.MINIMUM_SIZE_FOR_IMAGE_COMPRESSION_IN_BYTES
+    ) {
+      const compressedImagePath = await Compressor.compress(props.imagePath);
+      const compressedImageStats = await FileSystem.stat(compressedImagePath);
+      imageSize = compressedImageStats.size;
+      localImagePath = compressedImagePath;
+    }
+    /**
+     *  Skip Uploading Image If it doesn't meet limits
+     */
+    if (imageSize > WallpaperLimits.MAXIMUM_IMAGE_UPLOAD_SIZE_LIMIT_IN_BYTES) {
+      throw new Error(
+        `Maximum Image Size Limit Is ${WallpaperLimits.MAXIMUM_IMAGE_UPLOAD_SIZE_LIMIT_IN_MEGABYTE} MB`,
+      );
+    }
+
     await this._repo.uploadWallpaper({
       ...props,
-      size: stats.size,
-      imagePath: compressedImage,
+      size: imageSize,
+      imagePath: localImagePath,
     });
   }
 }
---
+++ src/utils/constants/limits.ts
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) Ronit Kr Shah
+ *
+ * The source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export const WallpaperLimits = {
+  MINIMUM_SIZE_FOR_IMAGE_COMPRESSION_IN_BYTES: 200000,
+  MINIMUM_SIZE_FOR_IMAGE_COMPRESSION_IN_KILO_BYTES: 200,
+  MAXIMUM_IMAGE_UPLOAD_SIZE_LIMIT_IN_BYTES: 1e6,
+  MAXIMUM_IMAGE_UPLOAD_SIZE_LIMIT_IN_MEGABYTE: 1,
+};
