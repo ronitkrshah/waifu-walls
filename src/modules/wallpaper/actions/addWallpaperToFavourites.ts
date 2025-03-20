488d0348a92448486bcdb5e7de021c1cad9420ea
RKS <ronitkrshah@tuta.io>
2025-03-21 01:23:56 +0530

    feat: remove storing base64 string on db while adding to favourites

--- src/modules/wallpaper/actions/addWallpaperToFavourites.ts
+++ src/modules/wallpaper/actions/addWallpaperToFavourites.ts
@@ -1,21 +1,14 @@
 import { db } from "~/database";
 import { Wallpaper } from "~/models";
-import { WallpaperService } from "~/services";
 
 export async function addWallpaperToFavourites(wallpaper: Wallpaper) {
-  let base64: string | null = null;
-  try {
-    base64 = await WallpaperService.getBase64FromImageURI(wallpaper.wallpaperUri);
-  } catch (e) {}
-
   await db.runAsync(
     `INSERT OR IGNORE INTO wallpapers 
-      (wallpaperId, wallpaperUri, wallpaperBase64, type, category, mimeType, extension) 
-      VALUES (?, ?, ?, ?, ?, ?, ?);`,
+      (wallpaperId, wallpaperUri, type, category, mimeType, extension) 
+      VALUES (?, ?, ?, ?, ?, ?);`,
     [
       wallpaper.wallpaperId,
       wallpaper.wallpaperUri,
-      base64,
       wallpaper.type,
       wallpaper.category as string,
       wallpaper.mimeType,
