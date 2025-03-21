bc03c7bb07fb56fb460227fc9be45b9e396a0ad0
RKS <ronitkrshah@tuta.io>
2025-03-22 00:08:12 +0530

    feat: add 2 more nsfw category types

--- src/api/types/WallpaperCategoryNSFW.ts
+++ src/api/types/WallpaperCategoryNSFW.ts
@@ -1,4 +1,6 @@
 export class WallpaperCategoryNSFW {
   public static readonly Waifu = "waifu";
   public static readonly Blowjob = "blowjob";
+  public static readonly Trap = "trap";
+  public static readonly Neko = "neko";
 }
