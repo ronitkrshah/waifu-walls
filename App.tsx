1d20885264cbcd145bc1a157ccc65713d7eff690
RKS <ronitkrshah@tuta.io>
2025-03-22 19:38:18 +0530

    feat: request notification permission

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ActivityLoaderProvider } from "~/components";
@@ -5,11 +6,21 @@ import { initializeDatabase, initialzeTables } from "~/database";
 import { RootNavigation } from "~/navigation";
 import { NotificationService } from "~/services";
 import { MaterialYouThemeProvider } from "~/theme";
+import * as Notifications from "expo-notifications"
 
 initializeDatabase().then(initialzeTables);
 NotificationService.setup();
 
 function App() {
+
+  useEffect(() => {
+    Notifications.getPermissionsAsync().then(status => {
+      if(!status.granted) {
+        status.canAskAgain && Notifications.requestPermissionsAsync()
+      }
+    })
+  }, [])
+
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView>
