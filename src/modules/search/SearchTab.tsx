1fdc1c9c00575bf85b7e6b807d43305bcaf7cf5f
RKS <ronitkrshah@tuta.io>
2025-03-23 18:35:21 +0530

    ui: update search tab ui

--- src/modules/search/SearchTab.tsx
+++ src/modules/search/SearchTab.tsx
@@ -1,91 +1,127 @@
-import { Fragment, useEffect, useState } from "react";
-import { SwipeableTabs } from "./components";
+import { Fragment, useMemo, useState } from "react";
 import { AppHeader } from "~/components";
-import { Dimensions, StyleSheet, View } from "react-native";
-import {
-  TWallpaperCategory,
-  TWallpaperType,
-  WallpaperCategoryNSFW,
-  WallpaperCategorySFW,
-} from "~/api";
-import { Button, MaterialBottomTabScreenProps } from "react-native-paper";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { TWallpaperType, WallpaperCategoryNSFW, WallpaperCategorySFW } from "~/api";
+import { Button, MaterialBottomTabScreenProps, useTheme } from "react-native-paper";
 import { ScrollView } from "react-native-gesture-handler";
 import { CompositeScreenProps } from "@react-navigation/native";
 import { TBottomTabNavigationRoutes, TStackNavigationRoutes } from "~/navigation";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import Animated, { FadeIn } from "react-native-reanimated";
+import Animated, { FadeIn, useAnimatedStyle, withTiming } from "react-native-reanimated";
 
 type TProps = CompositeScreenProps<
   MaterialBottomTabScreenProps<TBottomTabNavigationRoutes, "SearchTab">,
   NativeStackScreenProps<TStackNavigationRoutes>
 >;
 
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
 const AnimatedButton = Animated.createAnimatedComponent(Button);
 
 export default function SearchTab({ navigation }: TProps) {
-  const [sfwList, setSfwList] = useState<{ title: string; value: string }[]>([]);
-  const [nsfwList, setNsfwList] = useState<{ title: string; value: string }[]>([]);
+  const [activeType, setActiveType] = useState<TWallpaperType>("sfw");
+  const theme = useTheme();
 
-  useEffect(() => {
-    const sfw = Object.entries(WallpaperCategorySFW).map(([key, value]) => ({
-      title: key,
-      value,
-    }));
-    const nsfw = Object.entries(WallpaperCategoryNSFW).map(([key, value]) => ({
-      title: key,
-      value,
-    }));
-    setSfwList(sfw);
-    setNsfwList(nsfw);
-  }, []);
+  const ActiveKeys = useMemo(() => {
+    return Object.entries(activeType === "sfw" ? WallpaperCategorySFW : WallpaperCategoryNSFW).map(
+      ([key, value]) => ({
+        title: key,
+        value,
+      }),
+    );
+  }, [activeType]);
+
+  const rSfwBtnStyle = useAnimatedStyle(() => ({
+    backgroundColor: withTiming(
+      activeType === "sfw" ? theme.colors.primary : theme.colors.secondaryContainer,
+    ),
+  }));
+
+  const rSfwBtnTextStyle = useAnimatedStyle(() => ({
+    color: withTiming(
+      activeType === "sfw" ? theme.colors.onPrimary : theme.colors.onSecondaryContainer,
+    ),
+  }));
+
+  const rNsfwBtnStyle = useAnimatedStyle(() => ({
+    backgroundColor: withTiming(
+      activeType === "nsfw" ? theme.colors.primary : theme.colors.secondaryContainer,
+    ),
+  }));
+
+  const rNsfwBtnTextStyle = useAnimatedStyle(() => ({
+    color: withTiming(
+      activeType === "nsfw" ? theme.colors.onPrimary : theme.colors.onSecondaryContainer,
+    ),
+  }));
 
   return (
     <Fragment>
       <AppHeader title="Search" />
-      <SwipeableTabs buttonLabelOne="SFW" buttonLabelTwo="NSFW">
-        <ScrollView contentContainerStyle={styles.container}>
-          {sfwList.map((item, index) => (
+      <ScrollView contentContainerStyle={styles.contentContainer} stickyHeaderIndices={[0]}>
+        <View style={[styles.stickyHeader, { backgroundColor: theme.colors.surfaceDisabled }]}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={() => setActiveType("sfw")}>
+              <Animated.View style={[styles.button, rSfwBtnStyle]}>
+                <Animated.Text style={[rSfwBtnTextStyle, styles.buttonText]}>
+                  {" SFW "}
+                </Animated.Text>
+              </Animated.View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setActiveType("nsfw")}>
+              <Animated.View style={[styles.button, rNsfwBtnStyle]}>
+                <Animated.Text style={[rNsfwBtnTextStyle, styles.buttonText]}>
+                  {" NSFW "}
+                </Animated.Text>
+              </Animated.View>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.categoriesButtonContainer}>
+          {ActiveKeys.map((item, index) => (
             <AnimatedButton
               entering={FadeIn.delay(index * 50)}
               onPress={() => {
                 navigation.push("SearchResultsScreen", {
-                  wallpaperType: "sfw",
+                  wallpaperType: activeType,
                   wallpaperCategory: item.value,
                 });
               }}
-              key={"sfw_" + item.value}
+              key={activeType + item.value}
               mode={index % 2 === 0 ? "contained" : "contained-tonal"}
             >
               {item.title}
             </AnimatedButton>
           ))}
-        </ScrollView>
-        <ScrollView contentContainerStyle={styles.container}>
-          {nsfwList.map((item, index) => (
-            <AnimatedButton
-              entering={FadeIn.delay(index * 50)}
-              onPress={() => {
-                navigation.push("SearchResultsScreen", {
-                  wallpaperType: "nsfw",
-                  wallpaperCategory: item.value,
-                });
-              }}
-              key={"nsfw_" + item.value}
-              mode={index % 2 === 0 ? "contained" : "contained-tonal"}
-            >
-              {item.title}
-            </AnimatedButton>
-          ))}
-        </ScrollView>
-      </SwipeableTabs>
+        </View>
+      </ScrollView>
     </Fragment>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    width: SCREEN_WIDTH,
+  contentContainer: {
+    paddingHorizontal: 16,
+  },
+  stickyHeader: {
+    marginBottom: 16,
+    paddingVertical: 16,
+    borderRadius: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  button: {
+    borderRadius: 12,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    backgroundColor: "red",
+  },
+  buttonText: {
+    fontWeight: "600",
+    fontSize: 16,
+  },
+  categoriesButtonContainer: {
     flexDirection: "row",
     flexWrap: "wrap",
     alignItems: "center",
